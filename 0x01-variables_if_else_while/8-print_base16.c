83e1ada4d31911d7944e598054bc1e7279f67b78
folerejoice <jabulanifole@gmail.com>
2023-03-20 11:54:59 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 8-print_base16.c -o 8-

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  * Return: 0 (Success)
  */
+
 int main(void)
 {
        	int d;
 	char low;
 
-        for (d = '0'; d <= '; d++)
+        for (d = '0'; d <= '9'; d++)
 	putchar(d);
 
 	for (low = 'a'; low <= 'f'; low++)
